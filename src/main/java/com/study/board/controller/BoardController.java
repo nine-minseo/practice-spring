d58e35396f0474e902242edde5aaed176fa837f8
구민서 <minseo12379@gmail.com>
2025-09-09 15:22:32 +0900

    feat: 게시글 리스트 띄우는 페이지 구현

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.study.board.controller;
 
+
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,5 +26,12 @@ public class BoardController {
         boardService.write(board);
         
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardlist";
     }
 }
